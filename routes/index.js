18909cad94a8a66e86cca99581bada7aa36aec47
Toma Sauveton <thomas.sauveton@gmail.com>
2019-06-13 17:11:05 +0200

    creation du contenu dynamique pour les bikes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+var dataBike = [
+  {name: 'Model BIKO45', price: 679, url: '/images/bike-1.jpg'},
+  {name: 'Model ZOOK7', price: 799, url: '/images/bike-2.jpg'},
+  {name: 'Model LIKO89', price: 839, url: '/images/bike-3.jpg'},
+  {name: 'Model GEWO', price: 1206, url: '/images/bike-4.jpg'},
+  {name: 'Model TITAN5', price: 989, url: '/images/bike-5.jpg'},
+  {name: 'Model AMIG39', price: 599, url: '/images/bike-6.jpg'}
+];
+
+
+
+
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { dataBike });
 });
 
 
@@ -13,9 +27,10 @@ router.get('/shop', function(req, res, next) {
 });
 
 router.get('/product1', function(req, res, next) {
-  res.render('product1', { title: 'page produit !' });
+  res.render('product1', { dataBike});
 });
 
 
 
 module.exports = router;
+
