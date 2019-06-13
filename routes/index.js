4efc66759f009f8ebeb06fe0d2f319e40e610d33
Toma Sauveton <thomas.sauveton@gmail.com>
2019-06-13 16:01:33 +0200

    creation des routes shop & product

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+
+router.get('/shop', function(req, res, next) {
+  res.render('shop', { title: 'page panier!' });
+});
+
+router.get('/product1', function(req, res, next) {
+  res.render('product1', { title: 'page produit !' });
+});
+
+
+
 module.exports = router;
