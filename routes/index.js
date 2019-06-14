ccf2aad7395854feb5c45ea0d5a7f1cc93f7b2ae
Toma Sauveton <thomas.sauveton@gmail.com>
2019-06-14 16:14:53 +0200

    page produit update W/contenu dynamique mis en place

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 var dataBike = [
-  {name: 'Model BIKO45', price: 679, url: '/images/bike-1.jpg'},
-  {name: 'Model ZOOK7', price: 799, url: '/images/bike-2.jpg'},
-  {name: 'Model LIKO89', price: 839, url: '/images/bike-3.jpg'},
-  {name: 'Model GEWO', price: 1206, url: '/images/bike-4.jpg'},
-  {name: 'Model TITAN5', price: 989, url: '/images/bike-5.jpg'},
-  {name: 'Model AMIG39', price: 599, url: '/images/bike-6.jpg'}
+  {name: 'Model BIKO45', price: 679, url: '/images/bike-1.jpg',color:"red"},
+  {name: 'Model ZOOK7', price: 799, url: '/images/bike-2.jpg',color:"black & red"},
+  {name: 'Model LIKO89', price: 839, url: '/images/bike-3.jpg',color:"white"},
+  {name: 'Model GEWO', price: 1206, url: '/images/bike-4.jpg',color:"white & red"},
+  {name: 'Model TITAN5', price: 989, url: '/images/bike-5.jpg',color:"blue"},
+  {name: 'Model AMIG39', price: 599, url: '/images/bike-6.jpg',color:"red"}
 ];
 
 var dataCardBike = [
@@ -31,9 +31,10 @@ router.get('/shop', function(req, res, next) {
 });
 
 router.get('/product1', function(req, res, next) {
-
+  dataBike[req.query.position]
+  console.log( dataBike[req.query.position])
   // mettre une boucle ici qui permet de choisir lelement que l'on souhaite 
-  res.render('product1', { dataBike});
+  res.render('product1', { dataBike: dataBike[req.query.position]});
 });
 
 
